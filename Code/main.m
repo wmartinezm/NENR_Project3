472c9e310f7d79ede1cf433f433fbcf38bfd5ea1
wmartinezm <wlsn.martinez@gmail.com>
2023-10-25 19:50:11 -0600

    New Script for testing

--- Code/main.m
+++ Code/main.m
@@ -8,62 +8,276 @@ pw=200;%us
 freq=30;%Hz
 [rt,t]=ctrlArduinoStim_hrc('stim',[amp,pw,freq,1])
 
-%% Experiment section
+%% Experiment section Initialization
 
 % Define the experiment parameters
 current_threshold = 1; % mA (replace with actual current threshold)
 multiplier = 1.5;
-participants = 3;
+current_level = current_threshold + multiplier * current_threshold;
 
 % Define the range of pulse widths and frequencies to test
 pulse_widths = 50:50:250; % microseconds
 pulse_frequencies = 30:30:240; % Hz
 
+participants = 1;
+stimulation_duration = 1;
+
 % Initialize data storage
-results = zeros(length(pulse_widths), length(pulse_frequencies), participants);
+results_pw = zeros(length(pulse_widths), participants);
+results_freq = zeros(length(pulse_frequencies), participants);
+%% Test_1 PW = 50 us, f= 30 Hz
+pw_index = 1;
+pw = pulse_widths(pw_index);
+freq = pulse_frequencies(1);
+% Run stimulation
+ctrlArduinoStim_hrc('stim', [current_level, pw, freq, 1]);
 
-% Loop through participants
-for participant = 1:participants
-    % Determine the current level for this participant
-    current_level = current_threshold + multiplier * current_threshold;
-    
-    % Loop through pulse widths
-    for pw_index = 1:length(pulse_widths)
-        pw = pulse_widths(pw_index);
-        
-        % Loop through pulse frequencies
-        for freq_index = 1:length(pulse_frequencies)
-            freq = pulse_frequencies(freq_index);
-            
-            % Run stimulation
-            ctrlArduinoStim_hrc('stim', [current_level, pw, freq]);
-            
-            % Add a delay for stimulation duration
-            pause(stimulation_duration);
-            
-            % Stop the stimulation
-            ctrlArduinoStim_hrc('stim');
-            
-            % Prompt the participant to report perceived magnitude using user input
-            prompt = sprintf('Participant %d: Please report the perceived magnitude for this test: ', participant);
-            reported_magnitude = input(prompt);
-            
-            % Store the reported magnitude in the results matrix
-            % Replace 'reported_magnitude' with the actual value recorded
-            results(pw_index, freq_index, participant) = reported_magnitude;
-            
-            % Wait for the inter-trial interval
-            pause(inter_trial_interval);
-        end
-    end
-end
+% Add a delay for stimulation duration
+pause(stimulation_duration);
 
-% Perform data analysis and visualization as needed
-% Analyze the 'results' matrix to study the effect of pulse width and pulse frequency on perceived magnitude
-
-
-
-%%
+% Stop the stimulation
 ctrlArduinoStim_hrc('stim');
+% Prompt the participant to report perceived magnitude using user input
+prompt = sprintf('Please report the perceived magnitude for this test from 1 to 5(1 being mild and 5 strong perception):');
+reported_magnitude = input(prompt);
+
+% Store the reported magnitude in the results array
+results_pw(pw_index, participants) = reported_magnitude;
+
+%% Test_2 PW = 100 us, f= 30 Hz
+pw_index = 2;
+pw = pulse_widths(pw_index);
+freq = pulse_frequencies(1);
+% Run stimulation
+ctrlArduinoStim_hrc('stim', [current_level, pw, freq, 1]);
+
+% Add a delay for stimulation duration
+pause(stimulation_duration);
+
+% Stop the stimulation
+ctrlArduinoStim_hrc('stim');
+% Prompt the participant to report perceived magnitude using user input
+prompt = sprintf('Please report the perceived magnitude for this test from 1 to 5(1 being mild and 5 strong perception):');
+reported_magnitude = input(prompt);
+
+% Store the reported magnitude in the results array
+results_pw(pw_index, participants) = reported_magnitude;
+
+%% Test_3 PW = 150 us, f= 30 Hz
+pw_index = 3;
+pw = pulse_widths(pw_index);
+freq = pulse_frequencies(1);
+% Run stimulation
+ctrlArduinoStim_hrc('stim', [current_level, pw, freq, 1]);
+
+% Add a delay for stimulation duration
+pause(stimulation_duration);
+
+% Stop the stimulation
+ctrlArduinoStim_hrc('stim');
+% Prompt the participant to report perceived magnitude using user input
+prompt = sprintf('Please report the perceived magnitude for this test from 1 to 5(1 being mild and 5 strong perception):');
+reported_magnitude = input(prompt);
+
+% Store the reported magnitude in the results array
+results_pw(pw_index, participants) = reported_magnitude;
+
+%% Test_4 PW = 200 us, f= 30 Hz
+pw_index = 4;
+pw = pulse_widths(pw_index);
+freq = pulse_frequencies(1);
+% Run stimulation
+ctrlArduinoStim_hrc('stim', [current_level, pw, freq, 1]);
+
+% Add a delay for stimulation duration
+pause(stimulation_duration);
+
+% Stop the stimulation
+ctrlArduinoStim_hrc('stim');
+% Prompt the participant to report perceived magnitude using user input
+prompt = sprintf('Please report the perceived magnitude for this test from 1 to 5(1 being mild and 5 strong perception):');
+reported_magnitude = input(prompt);
+
+% Store the reported magnitude in the results array
+results_pw(pw_index, participants) = reported_magnitude;
+
+%% Test_5 PW = 250 us, f= 30 Hz
+pw_index = 5;
+pw = pulse_widths(pw_index);
+freq = pulse_frequencies(1);
+% Run stimulation
+ctrlArduinoStim_hrc('stim', [current_level, pw, freq, 1]);
+
+% Add a delay for stimulation duration
+pause(stimulation_duration);
+
+% Stop the stimulation
+ctrlArduinoStim_hrc('stim');
+% Prompt the participant to report perceived magnitude using user input
+prompt = sprintf('Please report the perceived magnitude for this test from 1 to 5(1 being mild and 5 strong perception):');
+reported_magnitude = input(prompt);
+
+% Store the reported magnitude in the results array
+results_pw(pw_index, participants) = reported_magnitude;
+
+%% Test_6 PW = 200 us, f= 30 Hz
+pw_index = 4;
+freq_index = 1;
+pw = pulse_widths(pw_index);
+freq = pulse_frequencies(freq_index);
+% Run stimulation
+ctrlArduinoStim_hrc('stim', [current_level, pw, freq, 1]);
+
+% Add a delay for stimulation duration
+pause(stimulation_duration);
+
+% Stop the stimulation
+ctrlArduinoStim_hrc('stim');
+% Prompt the participant to report perceived magnitude using user input
+prompt = sprintf('Please report the perceived magnitude for this test from 1 to 5(1 being mild and 5 strong perception):');
+reported_magnitude = input(prompt);
+
+% Store the reported magnitude in the results array
+results_freq(freq_index, participants) = reported_magnitude;
+
+%% Test_7 PW = 200 us, f= 60 Hz
+pw_index = 4;
+freq_index = 2;
+pw = pulse_widths(pw_index);
+freq = pulse_frequencies(freq_index);
+% Run stimulation
+ctrlArduinoStim_hrc('stim', [current_level, pw, freq, 1]);
+
+% Add a delay for stimulation duration
+pause(stimulation_duration);
+
+% Stop the stimulation
+ctrlArduinoStim_hrc('stim');
+% Prompt the participant to report perceived magnitude using user input
+prompt = sprintf('Please report the perceived magnitude for this test from 1 to 5(1 being mild and 5 strong perception):');
+reported_magnitude = input(prompt);
+
+% Store the reported magnitude in the results array
+results_freq(freq_index, participants) = reported_magnitude;
+
+%% Test_8 PW = 200 us, f= 90 Hz
+pw_index = 4;
+freq_index = 3;
+pw = pulse_widths(pw_index);
+freq = pulse_frequencies(freq_index);
+% Run stimulation
+ctrlArduinoStim_hrc('stim', [current_level, pw, freq, 1]);
+
+% Add a delay for stimulation duration
+pause(stimulation_duration);
+
+% Stop the stimulation
+ctrlArduinoStim_hrc('stim');
+% Prompt the participant to report perceived magnitude using user input
+prompt = sprintf('Please report the perceived magnitude for this test from 1 to 5(1 being mild and 5 strong perception):');
+reported_magnitude = input(prompt);
+
+% Store the reported magnitude in the results array
+results_freq(freq_index, participants) = reported_magnitude;
+
+%% Test_9 PW = 200 us, f= 120 Hz
+pw_index = 4;
+freq_index = 4;
+pw = pulse_widths(pw_index);
+freq = pulse_frequencies(freq_index);
+% Run stimulation
+ctrlArduinoStim_hrc('stim', [current_level, pw, freq, 1]);
+
+% Add a delay for stimulation duration
+pause(stimulation_duration);
+
+% Stop the stimulation
+ctrlArduinoStim_hrc('stim');
+% Prompt the participant to report perceived magnitude using user input
+prompt = sprintf('Please report the perceived magnitude for this test from 1 to 5(1 being mild and 5 strong perception):');
+reported_magnitude = input(prompt);
+
+% Store the reported magnitude in the results array
+results_freq(freq_index, participants) = reported_magnitude;
+
+%% Test_10 PW = 200 us, f= 150 Hz
+pw_index = 4;
+freq_index = 5;
+pw = pulse_widths(pw_index);
+freq = pulse_frequencies(freq_index);
+% Run stimulation
+ctrlArduinoStim_hrc('stim', [current_level, pw, freq, 1]);
+
+% Add a delay for stimulation duration
+pause(stimulation_duration);
+
+% Stop the stimulation
+ctrlArduinoStim_hrc('stim');
+% Prompt the participant to report perceived magnitude using user input
+prompt = sprintf('Please report the perceived magnitude for this test from 1 to 5(1 being mild and 5 strong perception):');
+reported_magnitude = input(prompt);
+
+% Store the reported magnitude in the results array
+results_freq(freq_index, participants) = reported_magnitude;
+
+%% Test_11 PW = 200 us, f= 180 Hz
+pw_index = 4;
+freq_index = 6;
+pw = pulse_widths(pw_index);
+freq = pulse_frequencies(freq_index);
+% Run stimulation
+ctrlArduinoStim_hrc('stim', [current_level, pw, freq, 1]);
+
+% Add a delay for stimulation duration
+pause(stimulation_duration);
+
+% Stop the stimulation
+ctrlArduinoStim_hrc('stim');
+% Prompt the participant to report perceived magnitude using user input
+prompt = sprintf('Please report the perceived magnitude for this test from 1 to 5(1 being mild and 5 strong perception):');
+reported_magnitude = input(prompt);
+
+% Store the reported magnitude in the results array
+results_freq(freq_index, participants) = reported_magnitude;
+%% Test_12 PW = 200 us, f= 210 Hz
+pw_index = 4;
+freq_index = 7;
+pw = pulse_widths(pw_index);
+freq = pulse_frequencies(freq_index);
+% Run stimulation
+ctrlArduinoStim_hrc('stim', [current_level, pw, freq, 1]);
+
+% Add a delay for stimulation duration
+pause(stimulation_duration);
+
+% Stop the stimulation
+ctrlArduinoStim_hrc('stim');
+% Prompt the participant to report perceived magnitude using user input
+prompt = sprintf('Please report the perceived magnitude for this test from 1 to 5(1 being mild and 5 strong perception):');
+reported_magnitude = input(prompt);
+
+% Store the reported magnitude in the results array
+results_freq(freq_index, participants) = reported_magnitude;
+
+%% Test_13 PW = 200 us, f= 240 Hz
+pw_index = 4;
+freq_index = 8;
+pw = pulse_widths(pw_index);
+freq = pulse_frequencies(freq_index);
+% Run stimulation
+ctrlArduinoStim_hrc('stim', [current_level, pw, freq, 1]);
+
+% Add a delay for stimulation duration
+pause(stimulation_duration);
+
+% Stop the stimulation
+ctrlArduinoStim_hrc('stim');
+% Prompt the participant to report perceived magnitude using user input
+prompt = sprintf('Please report the perceived magnitude for this test from 1 to 5(1 being mild and 5 strong perception):');
+reported_magnitude = input(prompt);
+
+% Store the reported magnitude in the results array
+results_freq(freq_index, participants) = reported_magnitude;
+
 %% close connection
 ctrlArduinoStim_hrc('close');
